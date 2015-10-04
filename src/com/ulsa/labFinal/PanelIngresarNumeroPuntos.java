c99f4c9daebd5af937ce58e0bfef242a36cd1245
dorlandimort <dorlandimort@hotmail.com>
2015-10-04 12:16:41 -0500

    Agregado el spinner al panel de ingresar puntos

--- src/com/ulsa/labFinal/PanelIngresarNumeroPuntos.java
+++ src/com/ulsa/labFinal/PanelIngresarNumeroPuntos.java
@@ -1,14 +1,12 @@
 package com.ulsa.labFinal;
 
-import java.awt.Color;
-import java.awt.Graphics;
-
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
 import javax.swing.border.Border;
 
 public class PanelIngresarNumeroPuntos extends JPanel {
@@ -32,21 +30,56 @@ public class PanelIngresarNumeroPuntos extends JPanel {
 		// Para dar tamaño a una ventana se usa setSize, no setBounds
 		this.setSize(300, 200);
 		
-		noPuntos = new JLabel("	No. Puntos :	");
+		noPuntos = new JLabel("No. Puntos:");
 		// falta el setbounds para noPuntos
+		noPuntos.setBounds(20, 50, 100, 20);
 		this.add(noPuntos);
-		spinner = new JSpinner();
+		SpinnerNumberModel modelo = new SpinnerNumberModel(2, 2, 100, 1);
+		spinner = new JSpinner(modelo);
 		// falta el setBounds para el spinner
-		spinner.setValue(30);
-		
+		spinner.setBounds(150, 50, 100, 20);
 		this.add(spinner);
 		
-		agregar = new JButton("Agregar", new ImageIcon("p.PNG"));
+		agregar = new JButton("Agregar");
 		// falta el setbounds para el boton Agregar
+		agregar.setBounds(150, 120, 100, 20);
 		this.add(agregar);
 		
 		
 	}
+
+	public Border getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(Border titulo) {
+		this.titulo = titulo;
+	}
+
+	public JSpinner getSpinner() {
+		return spinner;
+	}
+
+	public void setSpinner(JSpinner spinner) {
+		this.spinner = spinner;
+	}
+
+	public JLabel getNoPuntos() {
+		return noPuntos;
+	}
+
+	public void setNoPuntos(JLabel noPuntos) {
+		this.noPuntos = noPuntos;
+	}
+
+	public JButton getAgregar() {
+		return agregar;
+	}
+
+	public void setAgregar(JButton agregar) {
+		this.agregar = agregar;
+	}
+	
 	
 
 }
