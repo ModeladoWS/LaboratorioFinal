30ce18ebcbf3a3eca376795a62afebe1807849ac
dorlandimort <dorlandimort@hotmail.com>
2015-10-04 09:19:53 -0500

    Cambios

--- src/com/ulsa/labFinal/PanelDibujo.java
+++ src/com/ulsa/labFinal/PanelDibujo.java
@@ -2,13 +2,20 @@ package com.ulsa.labFinal;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.border.Border;
 
 public class PanelDibujo extends JPanel {
-	Border blackline = BorderFactory.createLineBorder(Color.black);
+	Border titulo = BorderFactory.createTitledBorder(null,"Plano");
+	
+	public static final int ANCHO = 500;
+	public static final int ALTO = 500;
+	static int escala = 15;
 	
 	public PanelDibujo() {
 		super.setBorder(blackline);
@@ -17,13 +24,29 @@ public class PanelDibujo extends JPanel {
 	}
 	
 	@Override
-	public void paint(Graphics g) {
-		super.paint(g);
+	public void paint(Graphics g){
+
+		Graphics2D g2 = (Graphics2D) g;
+
+		//Plano cartesiano
+		g2.drawLine(ConvertirUnidadesX(-50),ConvertirUnidadesY(0),ConvertirUnidadesX(50),ConvertirUnidadesY(0));
+		g2.drawLine(ConvertirUnidadesX(0),ConvertirUnidadesY(-50),ConvertirUnidadesX(0),ConvertirUnidadesY(50));
+
+	
 	}
 	
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		
+	}
+	private static int ConvertirUnidadesX(double x) {
+		double centro = ANCHO / 2.0;
+		return  (int)(centro + x * escala);
+	}
+
+	private static int ConvertirUnidadesY(double y) {
+		double centro = ALTO / 2.0;
+		return (int)(centro - y * escala);
 	}
 }
